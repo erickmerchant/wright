d25eea0b9addaa81d849b1a622a5888f84321143
Erick Merchant <erickmerchant@gmail.com>
2014-05-17 17:59:18 -0700

    Remove default converter

--- bin/includes/data.php
+++ bin/includes/data.php
@@ -1,24 +1,13 @@
 <?php namespace Wright\Data;
 
 use Symfony\Component\Yaml\Yaml;
-use Michelf\SmartyPants;
-use Michelf\MarkdownExtra;
 use Wright\Extensions\Twig;
-use Wright\Converter\MarkdownConverter;
 
 $container->bind(DataInterface::class, StandardData::class, [
 
         'data_filesystem' => $container->get('data_filesystem'),
 
         'yaml' => $container->definition(Yaml::class)
-    ])
-    ->after(function($data){
-
-        $markdown = new MarkdownConverter(new MarkdownExtra, new SmartyPants);
-
-        $data->addConverter('md', $markdown);
-
-        $data->addConverter('markdown', $markdown);
-    });
+    ]);
 
 $container->alias(DataInterface::class, 'data');
