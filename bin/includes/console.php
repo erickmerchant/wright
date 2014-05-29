81368dc090c738a6d174523f4aa7a9928351a0dd
Erick Merchant <erickmerchant@gmail.com>
2014-05-29 01:09:20 -0700

    Can do this instead

--- bin/includes/console.php
+++ bin/includes/console.php
@@ -1,18 +1,41 @@
 <?php namespace Wright;
 
-use Aura\Cli\CliFactory;
-
-$cli_factory = new CliFactory;
+use Aura\Cli\Stdio;
+use Aura\Cli\Context;
+use Aura\Cli\Stdio\Handle;
+use Aura\Cli\Stdio\Formatter;
 
 $container->bind(Application::class, Application::class, [
-        'stdio' => $container->resolvable(function () use ($cli_factory) {
+        'stdio' => $container->definition(Stdio::class, [
+            'stdin' => $container->resolvable(function(){
 
-            return $cli_factory->newStdio();
-        }),
-        'context' => $container->resolvable(function () use ($cli_factory) {
+                return new Handle('php://stdin', 'r');
+            }),
+            'stdout' => $container->resolvable(function(){
 
-            return $cli_factory->newContext($GLOBALS);
-        }),
+                return new Handle('php://stdout', 'w+');
+            }),
+            'stderr' => $container->resolvable(function(){
+
+                return new Handle('php://stderr', 'w+');
+            }),
+            'formatter' => $container->definition(Formatter::class)
+        ]),
+        'context' => $container->definition(Context::class, [
+            'env'    => $container->resolvable(function(){
+
+                return new Context\Env($_ENV);
+            }),
+            'server' => $container->resolvable(function(){
+
+                return new Context\Server($_SERVER);
+            }),
+            'argv'   => $container->resolvable(function(){
+
+                return new Context\Argv(isset($_SERVER['argv']) ? $_SERVER['argv'] : array());
+            }),
+            'getopt' => $container->definition(Context\Getopt::class)
+        ]),
         'commands' => [
             'publish' => $container->definition(Command\PublishCommand::class, [
                 'base_filesystem' => $container->get('base_filesystem'),
